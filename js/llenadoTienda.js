dda7ffe5be630746193b351761e937201fd2c63e
206 <206@academicos.cesde.edu.co>
2022-03-23 08:42:31 -0500

    se modifica esa vuelta

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -5,16 +5,16 @@ let productosBD=[
     /*{nombre:"camisaDipper",precio:50000, descripcion:"Camisa de color negro con diseño de Dipper"},
     {nombre:"camisaMabel",precio:50000, descripcion:"Camisa de color negro con diseño de Mabel"},*/
 
-    {foto: 'img/FunkoPopDipper.jpg' ,nombre:"Funkpop Dipper", precio:"$ "+124000+" COP", descripcion:"Funkopop de Dipper Pines clásico"},
-    {foto: 'img/termoAluminioPato.jpg', nombre:"Termo Aluminio Pato", precio:"$ "+28000+" COP", descripcion:"Termo de aluminio con diseño de Pato"},
-    {foto:'img/camisaPato.jpg', nombre:"Camisa Pato", precio:"$ "+35000+" COP", descripcion:"Camisa de color blanco con diseño de Pato"},
-    {foto: 'img/tulaDipper.jpg', nombre:"Tula Dipper", precio:"$ "+18000+" COP", descripcion:"Tula Deportiva blanca con diseño de Dipper"},
-    {foto: 'img/llaveroDipper.jpg',nombre:"Llavero Dipper", precio:"$ "+15000+" COP", descripcion:"Llavero con diseño Dipper"},
-    {foto: 'img/comic2GravityFalls.jpg',nombre:"Comic Gravity Falls dos", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Galls volumen dos"},
-    {foto: 'img/comic3GravityFalls.jpg',nombre:"Comic Gravity Falls tres", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Galls volumen tres"},
-    {foto: 'img/comic4GravityFalls.jpg',nombre:"Comic Gravity Falls cuatro", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Falls volumen cuatro"},
-    {foto: 'img/comic5GravityFalls.jpg',nombre:"Comic Gravity Falls cinco", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Falls volumen cinco"},
-    {foto: 'img/comic6GravityFalls.jpg',nombre:"Comic Gravity Falls seis", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Galls volumen seis"}
+    {foto: 'img/FunkoPopDipper.jpg' ,nombre:"Funkpop Dipper", precio:"$ "+124000+" COP", descripcion:"Funkopop de Dipper Pines clásico", boton: "Mostrar Producto"},
+    {foto: 'img/termoAluminioPato.jpg', nombre:"Termo Aluminio Pato", precio:"$ "+28000+" COP", descripcion:"Termo de aluminio con diseño de Pato", boton: "Mostrar Producto"},
+    {foto:'img/camisaPato.jpg', nombre:"Camisa Pato", precio:"$ "+35000+" COP", descripcion:"Camisa de color blanco con diseño de Pato", boton: "Mostrar Producto"},
+    {foto: 'img/tulaDipper.jpg', nombre:"Tula Dipper", precio:"$ "+18000+" COP", descripcion:"Tula Deportiva blanca con diseño de Dipper", boton: "Mostrar Producto"},
+    {foto: 'img/llaveroDipper.jpg',nombre:"Llavero Dipper", precio:"$ "+15000+" COP", descripcion:"Llavero con diseño Dipper", boton: "Mostrar Producto"},
+    {foto: 'img/comic2GravityFalls.jpg',nombre:"Comic Gravity Falls dos", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Galls volumen dos", boton: "Mostrar Producto"},
+    {foto: 'img/comic3GravityFalls.jpg',nombre:"Comic Gravity Falls tres", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Galls volumen tres", boton: "Mostrar Producto"},
+    {foto: 'img/comic4GravityFalls.jpg',nombre:"Comic Gravity Falls cuatro", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Falls volumen cuatro", boton: "Mostrar Producto"},
+    {foto: 'img/comic5GravityFalls.jpg',nombre:"Comic Gravity Falls cinco", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Falls volumen cinco", boton: "Mostrar Producto"},
+    {foto: 'img/comic6GravityFalls.jpg',nombre:"Comic Gravity Falls seis", precio:"$ "+35000+" COP", descripcion:"Comic Gravity Galls volumen seis", boton: "Mostrar Producto"}
 ]
 
 console.log(productosBD)
@@ -24,10 +24,10 @@ console.log(productosBD)
 //1. Creo una variable para lamacenar la base sobre la cual voy a pintar 
 let fila=document.getElementById("fila")
 productosBD.forEach(function(producto){
-    console.log(producto.nombre)
-    console.log(producto.precio) 
-    console.log(producto.foto)
-    console.log(producto.descripcion)
+    //console.log(producto.nombre)
+    //console.log(producto.precio) 
+    //console.log(producto.foto)
+    //console.log(producto.descripcion)
 
 
     //2. pintando etiquetas 
@@ -46,7 +46,7 @@ productosBD.forEach(function(producto){
     tarjeta.classList.add("h-100")
 
      //h4 con la clase text-center
-     let titulo=document.createElement("h5")
+     let titulo=document.createElement("h4")
      titulo.classList.add("text-center")
      titulo.textContent=producto.nombre
 
@@ -68,22 +68,17 @@ productosBD.forEach(function(producto){
     descripcion.textContent=producto.descripcion
 
     //tratar de hacer un boton 
-    /*let boton=document.createElement("button")
-    boton.classList("btn")
-    boton.classList("btn-info")
-    boton.textContent="Mostrar producto"*/
+    let boton=document.createElement("button")
+    boton.classList.add("btn","btn-info")
+    boton.textContent=producto.boton
 
-    const button=document.createElement("button")
-    button.type='button'
-    button.innerText="Mostrar producto"
 
     //padres e hijos 
     tarjeta.appendChild(titulo)
     tarjeta.appendChild(foto)
     tarjeta.appendChild(precio)
     tarjeta.appendChild(descripcion)
-    tarjeta.appendChild(button)
-    //tarjeta.appendChild(boton)
+    tarjeta.appendChild(boton)   
     columna.appendChild(tarjeta)
     fila.appendChild(columna)
 
@@ -92,3 +87,26 @@ productosBD.forEach(function(producto){
 
 })
 
+
+//rutina para ampliar información del producto 
+let filaContenedora=document.getElementById("fila")
+
+filaContenedora.addEventListener("click", function(evento){
+    if(evento.target.classList.contains("btn")){
+
+        console.log(evento.target.parentElement.querySelector("h5").textContent)
+        console.log(evento.target.parentElement.querySelector("img").src)
+
+        /*let fotoInfo=document.getElementById("fotoInfo")
+
+        fotoInfo.addEventListener("change",mostrarFoto)
+
+        function mostrarFoto () {
+            let foto=document.getElementById("fotoInfo")
+            foto.src=producto.foto
+        }*/
+
+        let modalInfo = new bootstrap.Modal(document.getElementById('modalInfo'))
+        modalInfo.show()
+    }
+})
